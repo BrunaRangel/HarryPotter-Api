1d23309b4dc5cf54881cf82b06220628485a9e43
Rangel <bruna.henrique.rangel@gmail.com>
2021-10-01 17:38:00 -0300

    Adding style to gryffindor

--- src/pages/Gryffindor.js
+++ src/pages/Gryffindor.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import { Header } from '../components/Header'
+import { Footer } from '../components/Footer'
 import { api } from '../services/api'
+import * as B from '../components/style/gryffindor'
+import flag  from '../images/Gryffindor.jpg'
 
 function Gryffindor() {
 
@@ -13,21 +16,21 @@ function Gryffindor() {
  },[])
 
   return (
-    <section>
+    <B.Container>
         <Header />
-        <div>
-            <div>
-                <h1>Grifinória</h1>
-            </div>
-            <div>
-                <p >"Quem sabe sua morada é a Grifinória</p>
-                <p>Casa onde habitam os corações indômitos.</p>
-                <p>Ousadia e sangue-frio e nobreza</p>
-                <p>Destacam os alunos da Grifinória dos demais."</p>
-                <p>— O Chapéu Seletor</p>
-            </div>  
-            <div>
-                <p>A Grifinória, fundada por Godrico Gryffindor, 
+        <B.WrapGryffindor>
+            <B.BoxTitle>
+                <B.Title>Grifinória</B.Title>
+            </B.BoxTitle>
+            <B.BoxVerse>
+                <B.Paragraph>"Quem sabe sua morada é a Grifinória</B.Paragraph>
+                <B.Paragraph>Casa onde habitam os corações indômitos.</B.Paragraph>
+                <B.Paragraph>Ousadia e sangue-frio e nobreza</B.Paragraph>
+                <B.Paragraph>Destacam os alunos da Grifinória dos demais."</B.Paragraph>
+                <B.Paragraph>— O Chapéu Seletor</B.Paragraph>
+            </B.BoxVerse>  
+            <B.BoxFoundation>
+                <B.Foundation>A Grifinória, fundada por Godrico Gryffindor, 
                     é uma das quatro casas da Escola de Magia e
                     Bruxaria de Hogwarts. Ao estabelecê-la, Godrico
                     instruiu o Chapéu Seletor a classificar estudantes
@@ -36,23 +39,19 @@ function Gryffindor() {
                     Suas cores são o escarlate e o ouro e seu animal
                      emblemático é um leão. Sir Nicholas de Mimsy-Porpington, 
                      também conhecido como "Nick Quase Sem Cabeça" 
-                     é o fantasma patrono da casa.</p>
-                <p>A casa corresponde aproximadamente ao elemento fogo,
+                     é o fantasma patrono da casa.</B.Foundation>
+                <B.Foundation>A casa corresponde aproximadamente ao elemento fogo,
                     e é por esta razão, que suas cores foram escolhidas;
                     elas também podem corresponder a um leão; com o escarlate
                     representando sua crina e cauda e o ouro simbolizando 
-                    sua pelagem.</p>
-            </div>    
-            <div>
-                {list.slice(0,3).map((item, index) => (
-                    <div key={index}>
-                        <img style={{width:'70px', height:'100px'}} src={item.image} alt=""  />
-                        <p>{item.name}</p>
-                    </div>
-                ))}
-            </div>    
-        </div>
-    </section>
+                    sua pelagem.</B.Foundation>
+            </B.BoxFoundation>  
+            <B.BoxImg>
+               <B.Img src={flag} alt="" />
+            </B.BoxImg>    
+        </B.WrapGryffindor>
+        <Footer />
+    </B.Container>
   )
 }
 
