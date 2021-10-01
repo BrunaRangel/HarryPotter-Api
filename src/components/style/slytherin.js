a1646a013e0085e6159dea0468c2490014142d60
Rangel <bruna.henrique.rangel@gmail.com>
2021-10-01 18:00:43 -0300

    Adding style to slytherin

---
+++ src/components/style/slytherin.js
@@ -0,0 +1,77 @@
+import styled from "styled-components";
+
+export const Container = styled.section`
+display: flex;
+align-items: center;
+justify-content: center;
+flex-direction: column;
+width:100%;
+background-color:#1a472a;
+`
+
+export const WrapSlytherin = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+flex-direction: column;
+width:100%;
+`
+
+export const BoxTitle = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+width:100%;
+height:15vh;
+`
+
+export const Title = styled.h1`
+color:#696969;
+font-size:4rem;
+&:hover {
+    font-size:4.5rem;
+    font-weight:700;
+    }
+`
+
+export const BoxVerse = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+flex-direction: column;
+gap:5px;
+`
+
+export const Paragraph = styled.p`
+color:#FFf;
+font-size:1.25rem;
+font-weight:600;
+line-height:0px;
+`
+
+export const BoxFoundation = styled.div`
+width:55%;
+display: flex;
+align-items: center;
+justify-content: center;
+flex-direction: column;
+gap:0px;
+margin-top:2rem;
+`
+
+export const Foundation = styled.p`
+color:#FFF;
+font-size:1.25rem;
+line-height:25px;
+text-indent:1.625rem;
+`
+
+export const BoxImg = styled.div`
+width:40vw;
+height:auto;
+margin-top:2rem;
+`
+export const Img = styled.img`
+width:100%;
+border-radius:100px;
+`
--- src/pages/Slytherin.js
+++ src/pages/Slytherin.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import { Header } from '../components/Header'
+import { Footer } from '../components/Footer'
 import { api } from '../services/api'
+import * as B from '../components/style/slytherin'
+import flag  from '../images/Slytherin.jpg'
 
 function Slytherin() {
 
@@ -13,21 +16,21 @@ function Slytherin() {
  },[])
 
   return (
-    <section>
+    <B.Container>
         <Header />
-        <div>
-            <div>
-                <h1>Sonserina</h1>
-            </div>
-            <div>
-                <p >"Ou quem sabe a Sonserina será a sua casa</p>
-                <p>E ali fará seus verdadeiros amigos,</p>
-                <p>Homens de astúcia que usam quaisquer meios</p>
-                <p>Para atingir os fins que antes colimaram."</p>
-                <p>— O Chapéu Seletor</p>
-            </div>  
-            <div>
-                <p>A Sonserina, fundada por Salazar Slytherin,
+        <B.WrapSlytherin>
+            <B.BoxTitle>
+                <B.Title>Sonserina</B.Title>
+            </B.BoxTitle>
+            <B.BoxVerse>
+                <B.Paragraph>"Ou quem sabe a Sonserina será a sua casa</B.Paragraph>
+                <B.Paragraph>E ali fará seus verdadeiros amigos,</B.Paragraph>
+                <B.Paragraph>Homens de astúcia que usam quaisquer meios</B.Paragraph>
+                <B.Paragraph>Para atingir os fins que antes colimaram."</B.Paragraph>
+                <B.Paragraph>— O Chapéu Seletor</B.Paragraph>
+            </B.BoxVerse>  
+            <B.BoxFoundation>
+                <B.Foundation>A Sonserina, fundada por Salazar Slytherin,
                     é uma das quatro casas da Escola de Magia 
                     e Bruxaria de Hogwarts. Ao estabelecer a casa, 
                     Salazar instruiu o Chapéu Seletor a escolher 
@@ -37,19 +40,19 @@ function Slytherin() {
                     liderança e ambição. Vários membros da Sonserina
                     possuem uma certa tendência em formar grupos, 
                     muitas vezes adquirindo líderes, o que exemplifica 
-                    ainda mais as qualidades ambiciosas de Slytherin.</p>
-                <p>O fundador, de acordo com o próprio Chapéu Seletor, 
+                    ainda mais as qualidades ambiciosas de Slytherin.</B.Foundation>
+                <B.Foundation>O fundador, de acordo com o próprio Chapéu Seletor, 
                     valorizava e favorecia estudantes de sangue puro.
                     Entretanto, qualquer aluno que possui um status 
-                    sanguíneo diferente, pode ser colocado na casa.</p>
-                <p>Seu animal emblemático é uma serpente e suas cores
+                    sanguíneo diferente, pode ser colocado na casa.</B.Foundation>
+                <B.Foundation>Seu animal emblemático é uma serpente e suas cores
                     são verde e prata. Há dois diretores notáveis 
                     da Sonserina; Horácio Slughorn que assumiu o 
                     cargo duas vezes (primeiro se aposentou em 1981 
                     e retornou como diretor em 1997 até pouco 
                     antes de 2016) e Severo Snape. O fantasma 
-                    patrono da casa é o Barão Sangrento.</p>
-                <p>A Sonserina corresponde aproximadamente ao elemento
+                    patrono da casa é o Barão Sangrento.</B.Foundation>
+                <B.Foundation>A Sonserina corresponde aproximadamente ao elemento
                     água, dado que as serpentes são comumente associadas 
                     aos mares e lagos na mitologia da Europa Ocidental, 
                     bem como por serem animais fisicamente fluidos e flexíveis.
@@ -60,19 +63,15 @@ function Slytherin() {
                     inteiramente "pura". Suas cores também se baseiam
                     nas águas ao redor de lagos — que por sua vez 
                     são frequentemente esverdeados — e prata para a 
-                    associação da água cinzenta da chuva.</p>
+                    associação da água cinzenta da chuva.</B.Foundation>
 
-            </div>    
-            <div>
-                {list.slice(3,4).map((item, index) => (
-                    <div key={index}>
-                        <img style={{width:'70px', height:'100px'}} src={item.image} alt=""  />
-                        <p>{item.name}</p>
-                    </div>
-                ))}
-            </div>    
-        </div>
-    </section>
+            </B.BoxFoundation>    
+            <B.BoxImg>
+               <B.Img src={flag} alt="" />
+            </B.BoxImg>    
+        </B.WrapSlytherin>
+        <Footer />
+    </B.Container>
   )
 }
 
